0349934e649cd3090f96aa015801ac8350575337
Oscar H <ohur1688@yahoo.com>
2020-06-30 11:45:42 -0700

    Update mdstaff_api_get_demographic.py

--- mdstaff_api_get_demographic.py
+++ mdstaff_api_get_demographic.py
@@ -2,43 +2,63 @@ import requests
 import mdstaff_api_const
 import sys
 sys.path.insert(1, '..\\util')
-sys.path.insert(2, 'C:\\OscarScripts\\util')
-import calendar_datetime
+sys.path.insert(2, "C:\\OscarScripts\\util")
+from calendar_datetime import get_today_date_yyyymmdd
+from calendar_datetime import get_today_wkday
 from shutil import copyfile
 import logging
+import os
+import csv
 
-def get_demographic(headers, provider_list):
+def get_demographic(headers, provider_list, instance):
     # mdstaff id list should be passed as a list
     mdstaff_names = {}
     mdstaff_sccids = {}
-
-    today_date = calendar_datetime.get_today_date_yyyymmdd()
-    output_file = "data/mdstaff_id_table_" + today_date + ".txt"
     
-    current_time = calendar_datetime.get_current_time()
+    today_date = get_today_date_yyyymmdd()
+    wkday = get_today_wkday()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    
+    if instance == mdstaff_api_const.inpatient_instance:
+        output_file = dir_path + "/data/mdstaff_id_table_inpt" + today_date + ".txt"
+        final_table = dir_path + "/data/mdstaff_id_table_inpt_final.txt"
+        
+    elif instance == mdstaff_api_const.ambulatory_instance:
+        output_file = dir_path + "/data/mdstaff_id_table_amb" + today_date + ".txt"
+        final_table = dir_path + "/data/mdstaff_id_table_amb_final.txt"
+        
+    log_file = dir_path + mdstaff_api_const.log_file
     
     logging.basicConfig(
-        filename = 'data/mdstaff_api.log', \
+        filename = log_file, \
         filemode = 'a', \
-        level = logging.DEBUG, \
+        level = logging.INFO, \
         format = "%(levelname)s %(name)s %(asctime)s %(lineno)s - %(message)s ")
         
-    logging.debug("Starting get demographic.  Current time = " + current_time)
+    logging.info(wkday + ". Starting get demographic.")
     
     output_file_obj = open(output_file, "w")
     output_file_obj.write("mdstaff_id|vmcid|name\n")
     counter = 1
+    logging.info(wkday + ". Getting each staff demographic data")
 
     for mdstaff_id in provider_list:
-        api_url = mdstaff_api_const.api_url + "demographic/" + mdstaff_id
-        logging.debug(api_url)
+        api_url = mdstaff_api_const.api_url + "/" + instance + "/demographic/" + mdstaff_id
+        
+        logging.debug(wkday + ". API URL: " + api_url)
         response = requests.get(api_url, headers = headers)
-        logging.debug(str(counter) + ": Get_demographic api return code: " + \
-              str(response.status_code))
+        
+        if response.status_code == 200:
+            pass
+        else:
+            logging.critical(wkday + ". API URL: " + api_url)
+            logging.critical(wkday + ". Get_demographic api return code: " + \
+                             str(response.status_code))
 
         response_json = response.json()
         name = response_json["FormattedName"]
         sccid = response_json["OtherID"]
+        email_add = response_json["Email"]
         mdstaff_names[mdstaff_id] = name
         mdstaff_sccids[mdstaff_id] = sccid
         counter = counter + 1
@@ -46,9 +66,13 @@ def get_demographic(headers, provider_list):
         output_file_obj.write(mdstaff_id + "|" + str(sccid) + "|" + name + "\n")
 
     output_file_obj.close()
-    current_time = calendar_datetime.get_current_time()
-    logging.debug("Completing get demographic.  Current time = " + current_time)
-    copyfile(output_file, "mdstaff_id_table_final.txt")
+    
+    logging.info(wkday + ". Completing all staff demographic data")
+    copyfile(output_file, final_table)
+    
+    return mdstaff_sccids, mdstaff_names
+    
+    
     
 def merge_id_table():
     
